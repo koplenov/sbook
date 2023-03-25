1e29134b86aeebd2d3e4cadecd12b95ab7818c34
koplenov <koplenov@gmail.com>
2023-03-26 00:23:01 +0300

    setup cors

--- main.js
+++ main.js
@@ -1,32 +1,36 @@
-const { sequelize, db_dto_story } = require('./db')
+const { sequelize, db_dto_story } = require('./db');
 
+const cors = require('@fastify/cors');
 const fastify = require('fastify')({
     logger: true
-})
+});
 
 fastify.get('/find/one/by/id/:id', function (request, reply) {
-    const { id } = request.params
+    const { id } = request.params;
     return db_dto_story.findOne({
         where: {
             id
         }
-    })
+    });
 })
 
 
 fastify.get('/find/all/by/city/:city', function (request, reply) {
-    const { city } = request.params
+    const { city } = request.params;
     return db_dto_story.findAll({
         where: {
             city
         }
-    })
+    });
 })
 
 const port = process.env.PORT || 3000;
 const start = async () => {
     try {
         await sequelize.sync();
+        await fastify.register(cors, {
+            hook: 'preHandler',
+        });
         await fastify.listen({ port });
     } catch (err) {
         fastify.log.error(err);
